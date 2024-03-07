05ba27d1bd3246d7803f04b558ce25262e123663
RJ-06 <54755468+RJ-06@users.noreply.github.com>
2024-03-07 09:45:41 -0800

    committing more stuff that aren't done
    see if you can access this

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody rb;
+    private Rigidbody rb; //control physics
     [SerializeField] Transform groundCheck;
+    [SerializeField] float groundCheckRadius;
+    [SerializeField] float JumpForce;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +19,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
+    }
+    
+    //fixedUpdate runs a fixed amount of times per second, so is used for physics
     private void FixedUpdate()
     {
+        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), rb.velocity.y, Input.GetAxisRaw("Vertical"));
+        SphereCollider s = new SphereCollider(groundCheck,groundCheckRadius,true);
+
+        rb.velocity = move;
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            rb.AddForce(new Vector3(0, JumpForce, 0));
+        }
         
     }
 }
