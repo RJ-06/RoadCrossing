7f696a0985b80e27e4fab1fb3768f6ee3fee696b
RJ-06 <54755468+RJ-06@users.noreply.github.com>
2024-03-14 09:23:01 -0700

    Car pathfinding
    Cars can now travel over a given path. Note - you will have to enter the starting node for a car in the editor, and will have to give each roadnode properties. Roadnodes allow you to give a car multiple different options of where to go (allowing straight or turns), and what speed the car goes at that point

---
+++ Assets/Scripts/RoadNode.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadNode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public RoadNode[] allPossiblePoints; //all connected points
+    public bool canMoveTo = true;
+    public float speed;
+
+    [SerializeField] float detectionRadius;
+    [SerializeField] SphereCollider s;
+
+    public List<RoadNode> tpossiblePoints; //true points the car can go to
+
+
+    void Start()
+    {
+        s = GetComponent<SphereCollider>();
+        s.radius = detectionRadius;
+        s.isTrigger = true;
+
+        foreach (RoadNode node in allPossiblePoints) //array of only places the player can move to
+        {
+            if(node.canMoveTo)
+                tpossiblePoints.Add(node);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
