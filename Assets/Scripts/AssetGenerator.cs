41182e3d8ce1b27cc7f439c2afef562cfe2247b4
RJ-06 <54755468+RJ-06@users.noreply.github.com>
2024-08-06 19:03:18 -0700

    asset generator
    make sure the assets list has the assets you want, set the radius for how far apart they can be, count of how many you want, and hit generate. Reset deletes all assets and resets array. Works in editor, can change assets in list depending on situation

--- Assets/Scripts/AssetGenerator.cs
+++ Assets/Scripts/AssetGenerator.cs
@@ -1,21 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 [ExecuteInEditMode]
-public class AssetGenerator : MonoBehaviour
+
+[CustomEditor(typeof(SceneDecoration))]
+public class AssetGenerator : Editor
 {
-    [SerializeField] (GameObject,float)[] decorations;
-    [SerializeField] float decoCap;
-    [SerializeField] float radius;
-    // Start is called before the first frame update
-    void Awake()
+
+    public override void OnInspectorGUI()
     {
-        while (decoCap > 0) 
+        base.OnInspectorGUI();
+
+        SceneDecoration sceneDecoration = (SceneDecoration)target;
+
+        sceneDecoration.count = (int)EditorGUILayout.Slider("Object count", sceneDecoration.count, 0, 40);
+        sceneDecoration.radius = EditorGUILayout.Slider("Placement radius", sceneDecoration.radius, 0, 10);
+
+        if (GUILayout.Button("Generate Assets"))
         {
-            var objectPlace = decorations[Random.Range(0, decorations.Length)];
-            GameObject obj = objectPlace[0];
-            var 
+            generateAssets(sceneDecoration);
+        }
+        if (GUILayout.Button("Reset Assets")) 
+        {
+            resetAssets(sceneDecoration);
         }
     }
+
+    void generateAssets(SceneDecoration s) 
+    {
+
+        for(int i = 0; i < s.count; i++) 
+        {
+            s.objects.Add(Instantiate(s.assets[Random.Range(0, s.assets.Length-1)],
+                new Vector3(s.transform.position.x + Random.Range(-s.radius,s.radius), s.transform.position.y, s.transform.position.z + Random.Range(-s.radius, s.radius)),
+                Quaternion.identity,s.transform));
+            s.objects[i].transform.Rotate(Vector3.up,Random.Range(0,360));
+        }
+    }
+
+    void resetAssets(SceneDecoration s) 
+    {
+        while (s.objects.Count > 0) 
+        {
+            DestroyImmediate(s.objects[0]);
+            s.objects.RemoveAt(0);
+        }
+
+
+        /*for(int i = s.count-1; i >=0; i++) 
+        {
+            var temp = s.objects[i];
+            s.objects[i] = null;
+            Destroy(temp);
+        }*/
+    }
+
 }
---
+++ Assets/Scripts/SceneDecoration.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneDecoration : MonoBehaviour
+{
+    public GameObject[] assets;
+    public List<GameObject> objects;
+    public float radius;
+    public int count;
+
+}
